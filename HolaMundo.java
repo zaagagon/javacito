332656194dd709df1d6a9ec6e4e9b3538afd43b1
zaagagon <55212969+zaagagon@users.noreply.github.com>
2021-08-14 23:54:44 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,7 @@
+//para usar la clase Scanner es necesario importar la biblioteca
+
+import java.util.Scanner;
+
 class HolaMundo{
 
 public static void main(String[] args){
@@ -5,6 +9,22 @@ public static void main(String[] args){
 //instrucción que mestra en pantalla 
 //mensaje y da salto de linea
 System.out.println("Hola mundo");
+  
+  //cambios realizados remotos freek
+  System.out.println("Digite su nombre por favor : ");
+  
+  String inputkeyboard="";//Creamos y asignamos la variable de entrada de teclado
+  
+  //creamos un objeto de Scanner
+  //invocamos el metodo nextLine() sobre el objeto inputo keyboard
+  
+  
+  Scanner inputscanner = new Scanner(System.in);
+  inputkeyboard=inputscanner.nextLine();
+
+  
+  
+  System.out.println("Bienvenido"+ inputkeyboard);
 
 }
 

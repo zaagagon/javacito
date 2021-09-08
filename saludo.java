d6e77e4d7760e7e89bb9be3da7d106293dc2337b
Alejandro Buendia Arango <zaagagon@gamail.com>
2021-09-07 20:49:49 -0500

    agregando archivos saludo java

---
+++ saludo.java
@@ -0,0 +1,12 @@
+//programa que imprime tabla
+public class saludo
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.println("Programando en java con git");
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}

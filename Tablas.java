1961501748ad92ecd9a4573d5f8c036b5dadfa21
System Administrator <zaagagon@gmail.com>
2021-11-24 18:21:00 -0500

    segunda prueba

--- Tablas.java
+++ Tablas.java
@@ -1,7 +1,8 @@
 public class Tablas {
     public static void main(String[] args) {
         //programa para imprimir la tabla del 8
-        System.out.println("programa que imprime la tabla del 8");
+        //Esta tabla tiene los datos quemados
+        System.out.println("Programa que imprime la tabla del 8");
         for(int i=1;i<=10;i++){
             
             System.out.println(i+"*"+"8"+"="+i*8);

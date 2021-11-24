dbcd1c941cc40eb2bf893130b8b5d6c2b0abcfd5
System Administrator <zaagagon@gmail.com>
2021-11-24 17:48:45 -0500

    tabla de multiplicar del 8
    ;

---
+++ Tablas.java
@@ -0,0 +1,12 @@
+public class Tablas {
+    public static void main(String[] args) {
+        //programa para imprimir la tabla del 8
+        System.out.println("programa que imprime la tabla del 8");
+        for(int i=1;i<=10;i++){
+            
+            System.out.println(i*8);
+
+        }
+        
+    }
+}

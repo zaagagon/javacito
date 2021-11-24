fd390a620b07c2dbde4865d9c9162a9da680fccb
System Administrator <zaagagon@gmail.com>
2021-11-24 17:36:14 -0500

    Retomando github con java

---
+++ SerieNumeros.java
@@ -0,0 +1,17 @@
+ 
+    
+    public class SerieNumeros {
+        public static void main(String[] args){
+            //bucle para imprimir numeros
+            // programa creado con github
+            for(int i=1;i<=10;i++){
+                System.out.println(i);
+            }
+            System.out.println("Numeros del 1 al 10");
+        }
+        
+       
+    }
+
+
+

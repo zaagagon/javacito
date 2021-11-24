6b04a47d387d1b7c5457be39b985f580aed57914
System Administrator <zaagagon@gmail.com>
2021-11-24 18:11:32 -0500

    prueba de acceso

--- Tablas.java
+++ Tablas.java
@@ -4,7 +4,7 @@ public class Tablas {
         System.out.println("programa que imprime la tabla del 8");
         for(int i=1;i<=10;i++){
             
-            System.out.println(i*8);
+            System.out.println(i+"*"+"8"+"="+i*8);
 
         }
         

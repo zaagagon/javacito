2417bf172e10c20708bd1a9633a8b8521e6d0968
System Administrator <zaagagon@gmail.com>
2021-11-24 21:24:43 -0500

    cambios en gitcofig

--- SerieNumeros.java
+++ SerieNumeros.java
@@ -3,7 +3,7 @@
     public class SerieNumeros {
         public static void main(String[] args){
             //bucle para imprimir numeros
-            // programa creado con github noviembre 2021 -- hulk cap
+            // programa creado con github noviembre 2021 -- hulk cap cambios en gitconfig
             for(int i=1;i<=10;i++){
                 System.out.println(i);
             }

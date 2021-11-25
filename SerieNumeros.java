ced54294131f9057c8f7237eeed95bbfb5d7796a
Alejandro Buendia Arango <zaagagon@gamail.com>
2021-11-24 20:07:09 -0500

    correct token

--- SerieNumeros.java
+++ SerieNumeros.java
@@ -3,7 +3,7 @@
     public class SerieNumeros {
         public static void main(String[] args){
             //bucle para imprimir numeros
-            // programa creado con github noviembre 2021
+            // programa creado con github noviembre 2021 -- mejorado con token y crendenciales
             for(int i=1;i<=10;i++){
                 System.out.println(i);
             }

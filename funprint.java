9d3b7df246c981eda9844aa6f1e3ad1d8eb33bbf
System Administrator <zaagagon@gmail.com>
2021-11-26 11:30:12 -0500

    funcion imprimir

---
+++ funprint.java
@@ -0,0 +1,16 @@
+
+public class funprint {
+
+
+    public static void main(String[] args) {
+        //llamado de la funcion desde el main
+        imprimir();
+    }
+
+    //funcion que imprime mensaje
+    //crear funcion para no llamar System.out y entregar parametro
+    public static void imprimir() {
+        System.out.println("Hola ZAG");
+        
+    }
+}

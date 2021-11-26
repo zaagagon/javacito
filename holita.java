0959ad95d8b56136da32f597a019e8600ed7b79c
System Administrator <zaagagon@gmail.com>
2021-11-26 11:11:23 -0500

    revision java

---
+++ holita.java
@@ -0,0 +1,15 @@
+//nombre de la clase
+public class holita { //archivo y clase deben tener el mismo nombre
+
+    //definir o crear el metodo principal
+    public static void main(String[] args) {
+        //instruccion 
+        //imprimir en pantalla hola
+        for(int i=1;i<=3;i++){
+            System.out.println("Hola a todos");
+        }
+        
+        
+    }
+    
+}

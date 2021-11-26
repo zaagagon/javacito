5abfaf14330889b7ae92e2cdb2ae280d8691a63a
System Administrator <zaagagon@gmail.com>
2021-11-26 12:05:16 -0500

    funcion sumar numeros falta imprimir

---
+++ FuncionSuma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FuncionSuma {
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+
+        System.out.println("Digite primer numero");
+        int a= entrada.nextInt();
+        System.out.println("Digite segundo numero");
+        int b= entrada.nextInt();
+        suma(a, b);
+    }
+    //funcion suma
+    public static void suma(int a,int b) {
+       // int a,b,c;
+        int c= a+b;
+    
+    }
+}

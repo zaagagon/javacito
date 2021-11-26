d81c75f8023de13a5670790dcaf0cea39d287c16
System Administrator <zaagagon@gmail.com>
2021-11-25 19:39:30 -0500

    entrada con Scanner

---
+++ fullname.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class fullname {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite su nombre completo : ");
+        //usamos en esta ocasion nextline para recibir
+        // los espacios en blanco ingresados por teclado
+        String completo=teclado.nextLine();
+        System.out.println("Bienvenido " + completo);
+    }
+}

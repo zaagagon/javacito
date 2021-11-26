a6982c093a8ef0887f993db4b60ddee1e11bb9e8
System Administrator <zaagagon@gmail.com>
2021-11-26 12:20:13 -0500

    comentarios y maquetacion del programa

--- FuncionSuma.java
+++ FuncionSuma.java
@@ -1,13 +1,18 @@
+//importamos el paquete util
 import java.util.Scanner;
 
 public class FuncionSuma {
     public static void main(String[] args) {
+
+        //ingreso de datos
         Scanner entrada= new Scanner(System.in);
 
         System.out.println("Digite primer numero");
+        //tomamos los datos ingresados por teclado
         int a= entrada.nextInt();
         System.out.println("Digite segundo numero");
         int b= entrada.nextInt();
+        //llamado del metodo o funcion suma
         suma(a, b);
     }
     //funcion suma
@@ -15,7 +20,8 @@ public class FuncionSuma {
        // int a,b,c;
         int c= a+b;
         //imprimir el valor de la operacion
-        System.out.print(c);
+        //para quitar el % de la salida es posible con el salto de linea
+        System.out.print("El resultado de la operacion es : " + c +"\n");
     
     }
 }

40449324a45a78f5ee70ccad5d9137dc70814a66
System Administrator <zaagagon@gmail.com>
2021-11-27 14:46:43 -0500

    codigo para limpiar pantalla en java

--- Array.java
+++ Array.java
@@ -1,11 +1,18 @@
 public class Array {
     public static void main(String[] args) {
+        //Aquellos que me han preguntado para limpiar  la consola de salida
+        System.out.print("\033[H\033[2J");
+
+        System.out.flush();
         //Los arreglos se declara y se inicializan
         //Podemos hacer ambas cosas al tiempo
         //un ejemplo con un array bidimensional
 
         int [][] numeros={{9,8,7},{6,5,4},{3,2,1}};
-        System.out.println("\n"+" Arreglo declarado e inicializado : ");
-        System.out.println("int [][] numeros={{9,8,7},{6,5,4},{3,2,1}};");
+        System.out.println("\n"+" Arreglo declarado e inicializado : "+ "\n");
+        System.out.println("int [][] numeros={{9,8,7},{6,5,4},{3,2,1}};"+ "\n");
+        //Imprimir el arreglo recorriendo con for
+
+
     }
 }

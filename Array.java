4ccdb100902a8e30f78296eb9235d09207273e6a
System Administrator <zaagagon@gmail.com>
2021-11-27 10:30:45 -0500

    teoria arreglos

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        //Los arreglos se declara y se inicializan
+        //Podemos hacer ambas cosas al tiempo
+        //un ejemplo con un array bidimensional
+
+        int [][] numeros={{9,8,7},{6,5,4},{3,2,1}};
+        System.out.println("\n"+" Arreglo declarado e inicializado : ");
+        System.out.println("int [][] numeros={{9,8,7},{6,5,4},{3,2,1}};");
+    }
+}

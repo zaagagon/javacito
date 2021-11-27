be5ca9a3c54e99b9ebc3de5a2bc028500c6211a8
System Administrator <zaagagon@gmail.com>
2021-11-27 00:32:58 -0500

    astericos triangulo inverso

---
+++ aster2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+/*
+Razonamiento
+posicion (i,j) fila i columna j
+*(1,1)
+* *{(2,1),{2,2}} i es 2 y j cambia aqui usamos el segundo for
+
+*/
+
+public class aster2{
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in); 
+        System.out.println("Numero de filas del triangulo");
+        int fila= teclado.nextInt(); 
+        //solucion con dos for
+        for(int i=1;i<=fila;i++){
+            //fila--;
+         // System.out.println(fila);
+         //Solucion del triangulo inverso fila -i
+            for(int j=1;j<=fila-i;j++){
+                
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+
+    
+}
+
+

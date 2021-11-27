2ab16552011dc2138677e3bcf117821a5990b23e
System Administrator <zaagagon@gmail.com>
2021-11-27 11:45:48 -0500

    funcion printer y llamado la funcion

---
+++ CallPrinter.java
@@ -0,0 +1,8 @@
+//En este programa vamos a llamar la funcion printer
+
+public class CallPrinter {
+    public static void main(String[] args) {
+        Printer.printer("hola");
+    }
+   
+}
---
+++ Printer.java
@@ -0,0 +1,20 @@
+public class Printer {
+    //se me ocurre crear una funcion
+    //que nos evite digitar System.out.println("");
+    //Miremos
+
+    public static void main(String[] args) {
+        String w="Bienvenido ZAG ";
+        printer("hola"+w);
+        
+    }
+//funcion imprimir
+//Una vez creada esta funcion la podemos usar desde otros programas 
+//En otros ejemplos revisaremos
+    public static void printer(String C) {
+
+ System.out.println(C);
+        
+    }
+    
+}

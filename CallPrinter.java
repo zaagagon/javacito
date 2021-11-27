09637b19981576e6296d6b8c578bdb6b3bedd5fa
System Administrator <zaagagon@gmail.com>
2021-11-27 11:48:10 -0500

    comentarios

--- CallPrinter.java
+++ CallPrinter.java
@@ -2,6 +2,8 @@
 
 public class CallPrinter {
     public static void main(String[] args) {
+        //llamado del metodo o funcion printer de la clase printer
+        //Evaluemos el metodo con un ingreso por teclado y concatenado
         Printer.printer("hola");
     }
    

0083bc7b8fd975824523e51774111f64785dd203
System Administrator <zaagagon@gmail.com>
2021-11-29 23:57:09 -0500

    comentarios variables

--- Suma2.java
+++ Suma2.java
@@ -8,12 +8,14 @@ public class Suma2 {
         int n1 = teclado.nextInt();
         //la funcion printer recibe enteros
         Printer.printer(n1);
+        //declaramos e inicializamos n2
         int n2=6;
         //se llama el metodo suma
         suma(n1, n2);
     }
 
     public static void suma(int n1,int n2) {
+        //Declaramos e inicializamos la variable C
         int c = n1+n2;
         //usamos return
     Printer.printer(c);

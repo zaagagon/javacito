2fdb3a9a4ee645920957d5de44436791182af10d
System Administrator <zaagagon@gmail.com>
2021-11-30 23:59:47 -0500

    hoa

--- saludo.java
+++ saludo.java
@@ -1,9 +1,11 @@
 //programa que imprime tabla
 public class saludo
 {
-    public static void main(String args[])
+    //metodo principal
+        public static void main(String args[])
     {
         for(int i = 0; i <= 12; i++)
+        //tabla del 12?
         {
             System.out.println("Programando en java con git");
             System.out.print("12 * "+ i + " = " + 12 * i + "\n");

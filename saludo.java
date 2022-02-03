e9c1757dcf830e360e328244c3427f14822930b1
System Administrator <zaagagon@gmail.com>
2022-02-02 23:42:02 -0500

    correciones

--- saludo.java
+++ saludo.java
@@ -4,10 +4,11 @@ public class saludo
     //metodo principal
         public static void main(String args[])
     {
+        System.out.println("Programando en java con git");
         for(int i = 0; i <= 12; i++)
         //tabla del 12?
         {
-            System.out.println("Programando en java con git");
+            
             System.out.print("12 * "+ i + " = " + 12 * i + "\n");
         }
     }

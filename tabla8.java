76b72d9f47083ea56d4b840dd6d950a8160d56af
System Administrator <zaagagon@gmail.com>
2022-02-02 23:39:56 -0500

    solucion

--- tabla8.java
+++ tabla8.java
@@ -1,11 +1,13 @@
 //PROGRAMA QUE IMPRIME LA TABLA DEL 8
 
+//la clase tiene el mismo nombre del archivo java
 public class tabla8 {
     
     public static void main(String[] args) {
         //programa para imprimir la tabla del 8
         //Esta tabla tiene los datos quemados
         System.out.println("Programa que imprime la tabla del 8");
+        //solucion
         for(int i=1;i<=10;i++){
             
             System.out.println(i+"*"+"8"+"="+i*8);

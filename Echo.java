dc72b776eafc746926d5db018c8671c50f06bb41
System Administrator <zaagagon@gmail.com>
2022-02-02 23:27:16 -0500

    echo.java

--- Echo.java
+++ Echo.java
@@ -3,7 +3,7 @@ public class Echo {
 public static void main (String[] args) {
 BufferedReader entradaEstandar = new BufferedReader (new InputStreamReader(System.in));
 String mensaje;
-System.out.println ("Introducir una linnea de texto:");
+System.out.println ("Introducir una linea de texto:");
  mensaje = entradaEstandar.readLine();
  System.out.println ("Introducido: \"" + mensaje + "\"");
 } }

0003c643aba85b36c5f36b886bf219c474aa6243
System Administrator <zaagagon@gmail.com>
2022-02-03 23:31:19 -0500

    tabla9

---
+++ tabla9.java
@@ -0,0 +1,19 @@
+//PROGRAMA QUE IMPRIME LA TABLA DEL 8
+
+//la clase tiene el mismo nombre del archivo java
+public class tabla7 {
+    
+    public static void main(String[] args) {
+        //programa para imprimir la tabla del 8
+        //Esta tabla tiene los datos quemados
+        System.out.println("Programa que imprime la tabla del 8");
+        //solucion
+        System.out.println("Tabla del 7");
+        for(int i=1;i<=10;i++){
+            
+            System.out.println(i+"*"+"7"+"="+i*7);
+
+        }
+        
+    }
+}

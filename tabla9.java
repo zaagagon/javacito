28212502d60fe8b380abe9e7996178aa7fb937a1
System Administrator <zaagagon@gmail.com>
2022-02-03 23:32:54 -0500

    operacion 9

--- tabla9.java
+++ tabla9.java
@@ -11,7 +11,7 @@ public class tabla7 {
         System.out.println("** Tabla del 9 **");
         for(int i=1;i<=10;i++){
             
-            System.out.println(i+"*"+"7"+"="+i*7);
+            System.out.println(i+"*"+"7"+"="+i*9);
 
         }
         

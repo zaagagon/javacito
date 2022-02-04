ee079c04408c0fe9260ad4a2cb979149595f18d4
System Administrator <zaagagon@gmail.com>
2022-02-03 23:53:41 -0500

    error corregido throws

--- Echo.java
+++ Echo.java
@@ -1,6 +1,6 @@
  import java.io.*;
 public class Echo {
-public static void main (String[] args) {
+public static void main (String[] args) throws IOException {
 BufferedReader entradaEstandar = new BufferedReader (new InputStreamReader(System.in));
 String mensaje;
 System.out.println ("Introducir una linea de texto:");

c0ac35255bc99429205b0a5e23f64fd50b240c82
System Administrator <zaagagon@gmail.com>
2022-02-03 23:55:48 -0500

    instancia bufferedReader

--- Echo.java
+++ Echo.java
@@ -2,6 +2,7 @@
 public class Echo {
     //error corregido con throws IOException
 public static void main (String[] args) throws IOException {
+    //creamos un instancia
 BufferedReader entradaEstandar = new BufferedReader (new InputStreamReader(System.in));
 String mensaje;
 System.out.println ("Introducir una linea de texto:");

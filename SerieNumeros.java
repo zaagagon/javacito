4b7a88cc2372c729048dfea3cc70377ecee14208
System Administrator <zaagagon@gmail.com>
2022-02-04 23:33:49 -0500

    titulo programa

--- SerieNumeros.java
+++ SerieNumeros.java
@@ -4,12 +4,14 @@
         public static void main(String[] args){
             //bucle para imprimir numeros
             // programa creado con github noviembre 2021 -- hulk cap cambios en gitconfig--
+            System.out.println("Numeros del 1 al 10");
             for(int i=1;i<=10;i++){
                 System.out.println(i);
             }
             //imprime los numeros del 1 al 10
             System.out.println("Programa que imprime serie 1 al 10");
-            System.out.println("Numeros del 1 al 10");
+
+           
         }
         
        

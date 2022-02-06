1cac95a18ef9af27b9ae525eaa92be47fe225f70
System Administrator <zaagagon@gmail.com>
2022-02-05 23:47:34 -0500

    cuadro2

---
+++ Square2.java
@@ -0,0 +1,34 @@
+//programa que imprime cuadro de asteriscos
+
+import java.util.Scanner;
+
+//razonamiento de cuadrado de asteriscos
+/* 
+* *
+* *
+
+llenamos por filas o por columnas
+
+*/
+
+public class Square {
+    public static void main(String[] args) {
+        //instancia entrada
+        Scanner entrada= new Scanner(System.in);
+        System.out.println("programa que dibuha cuadro de asteriscos");
+        System.out.println("Digite el numero de astericos :");
+        //solucion con dos for
+        int fila = entrada.nextInt();
+        System.out.println();
+        for(int i=1;i<=fila;i++){
+         
+            for(int j=1;j<=fila;j++){
+                System.out.print(" @  ");
+               
+            }
+            System.out.println("\n");
+        }
+
+    }
+    
+}

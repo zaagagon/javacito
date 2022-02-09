044c176ad029a601b56d965177470924b644bcf2
System Administrator <zaagagon@gmail.com>
2022-02-08 23:51:04 -0500

    iteracion con nombre

--- holita.java
+++ holita.java
@@ -7,12 +7,13 @@ public class holita { //archivo y clase deben tener el mismo nombre
     public static void main(String[] args) {
         //instruccion 
         //imprimir en pantalla hola
-        for(int i=1;i<=3;i++){
-            Scanner nombre = new Scanner(System.in);
+        Scanner nombre = new Scanner(System.in);
             
-            System.out.println("Hola, Digite su nombre por favor : ");
-            String name =nombre.nextLine();
-          System.out.println(name + "Bievenido");
+        System.out.println("Hola, Digite su nombre por favor : ");
+        String name =nombre.nextLine();
+        for(int i=1;i<=3;i++){
+          
+          System.out.println(name + " Bievenido ");
         }
         
         

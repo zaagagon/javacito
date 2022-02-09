c67660ab7316df1f6fff4143176304fd0468a49d
System Administrator <zaagagon@gmail.com>
2022-02-08 23:55:01 -0500

    codigo limpio

--- holita.java
+++ holita.java
@@ -5,12 +5,16 @@ public class holita { //archivo y clase deben tener el mismo nombre
 
     //definir o crear el metodo principal
     public static void main(String[] args) {
+
         //instruccion 
         //imprimir en pantalla hola
+
         Scanner nombre = new Scanner(System.in);
             
         System.out.println("Hola, Digite su nombre por favor : " + "\n");
+
         String name = nombre.nextLine();
+        
         for(int i=1;i<=3;i++){
 
             //imprime la entrada 3 veces

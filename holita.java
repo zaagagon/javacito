5230e9133a582d25503920c8d7108de011c6f011
System Administrator <zaagagon@gmail.com>
2022-02-08 23:54:04 -0500

    commit

--- holita.java
+++ holita.java
@@ -12,6 +12,8 @@ public class holita { //archivo y clase deben tener el mismo nombre
         System.out.println("Hola, Digite su nombre por favor : " + "\n");
         String name =nombre.nextLine();
         for(int i=1;i<=3;i++){
+
+            //imprime la entrada 3 veces
           
           System.out.println("hello " + name + " Bievenido "+ "\n");
         }

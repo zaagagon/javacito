b92f5bda499fa323800f58fa29852397bd87173d
System Administrator <zaagagon@gmail.com>
2022-02-10 23:51:11 -0500

    hellograf

---
+++ javascript/hellograf.java
@@ -0,0 +1,20 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class hellograf {
+
+  public static void main(String[] args) {
+
+    JFrame ventana = new JFrame("Ventana Principal");
+    JPanel panel = new JPanel();
+    JLabel etiqueta = new JLabel("¡ Hola Mundo !");
+
+    panel.add(etiqueta);
+    ventana.setContentPane(panel);
+    ventana.setSize(320,200);
+    ventana.setVisible(true);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}

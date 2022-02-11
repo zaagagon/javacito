58bcd7d0ab31ea071882b102c212e0635778bd66
System Administrator <zaagagon@gmail.com>
2022-02-10 23:55:42 -0500

    example 2

---
+++ javascript/hellograf2.java
@@ -0,0 +1,22 @@
+import javax.swing.BorderFactory; //a
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+ 
+class HolaMundo {    //b
+ 
+    public static void main(String[] args) {                     //c
+        JFrame frame = new JFrame("Hola Mundo en Swing");        //d
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //e
+ 
+        JLabel label = new JLabel("Hola Mundo", JLabel.CENTER);                //f
+        Border empty = BorderFactory.createEmptyBorder(100, 100, 100, 100);    //g
+        label.setBorder(empty);                                                //h
+ 
+        frame.getContentPane().add(label);    //i
+ 
+        frame.pack();                         //j
+        frame.setLocationRelativeTo(null);    //k
+        frame.setVisible(true);               //l
+    }
+}

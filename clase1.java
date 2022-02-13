33cea1ae951fb36c5ff0822b72e92ba684e50103
System Administrator <zaagagon@gmail.com>
2022-02-12 23:36:32 -0500

    suma de variables

--- clase1.java
+++ clase1.java
@@ -2,6 +2,13 @@ public class clase1 {
 
     public static void main(String[] args) {
         System.out.println("prueba");
+        int a = 4;
+        int b =6;
+
+        System.out.println(a+b);
+         
+        
+
     }
     
 }

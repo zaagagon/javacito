1087df74172186409eef2115d8043d456f8fc26d
System Administrator <zaagagon@gmail.com>
2022-02-13 23:54:47 -0500

    cambios

---
+++ clase2.java
@@ -0,0 +1,32 @@
+//
+import java.util.*;
+
+public class clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("prueba");
+        //int a = 4;
+        int b =6;
+   
+
+        System.out.println(" **Programa que suma dos numeros *");
+       //objeto instancia teclado tipo Scanner
+       Scanner teclado = new Scanner(System.in);
+       System.out.println("cual es su nombre :");
+       String nombre = teclado.nextLine();
+       System.out.println("Digite dos numeros : ");
+       //llamamos la instancia teclado
+       int X=teclado.nextInt();
+       int Y=teclado.nextInt();
+
+       //variable resultado
+       int Z = X+Y;
+
+
+        System.out.println(nombre + " el Resultado de la suma : " + Z);
+         
+        
+
+    }
+    
+}

0710938b48e80c4f22f0bfdc94d5c09d044ecf8b
System Administrator <zaagagon@gmail.com>
2022-02-14 23:56:43 -0500

    comentarios

--- clase3.java
+++ clase3.java
@@ -1,7 +1,8 @@
 //
 import java.util.*;
 
-public class clase2 {
+//archivo y clase mismo nombre
+public class clase3 {
 
     public static void main(String[] args) {
         System.out.println("prueba");

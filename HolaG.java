7b867e79b264cb639a939c9b68cd610c291ca80b
System Administrator <zaagagon@gmail.com>
2022-04-01 20:38:26 -0500

    joption java

---
+++ HolaG.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class HolaG{
+ 
+ public static void main(String args[]){
+  
+  String nombre;
+  nombre = JOptionPane.showInputDialog("¿Cual es tu nombre?");
+
+  String saludo = "Hola " + nombre; 
+
+  JOptionPane.showMessageDialog(null, saludo);
+  
+  System.out.println("Hola "+nombre);
+ }
+}

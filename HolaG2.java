ac2f93938876e5ec211f5a65399bd143a99de9ab
System Administrator <zaagagon@gmail.com>
2022-04-04 11:01:07 -0500

    clase rectangulo

--- HolaG2.java
+++ HolaG2.java
@@ -2,7 +2,19 @@ import javax.swing.JOptionPane;
 
 public class HolaG2{
 	public static void main(String[] args) {
-		String name = JOptionPane.showInputDialog("Type your name please");
+		String name = JOptionPane.showInputDialog("Digite su nombre","datos");
 		JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        // Con JCombobox
+Object seleccion = JOptionPane.showInputDialog(
+    unComponentePadre,
+    "Seleccione opcion",
+    "Selector de opciones",
+    JOptionPane.QUESTION_MESSAGE,
+    unIcono,  // null para icono defecto
+    new Object[] { "opcion 1", "opcion 2", "opcion 3" }, 
+    "opcion 1");
+ 
+ System.out.println("El usuario ha elegido "+seleccion);
 	}
 }
---
+++ retorno.java
@@ -0,0 +1,13 @@
+//cmd + b para ventana completa codigo
+public class retorno {
+
+    public static void main(String[] args) {
+        
+    }
+
+    //creamos la clase rectangulo
+    class rectangulo {
+        
+    }
+    
+}

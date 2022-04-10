3d5395baf6a1be2925affdbcccc1a35909b5756a
System Administrator <zaagagon@gmail.com>
2022-04-10 00:06:45 -0500

    prueba operaciones objetos

--- PruebaSuma.java
+++ PruebaSuma.java
@@ -7,7 +7,10 @@ public class PruebaSuma {
         Printer b= new Printer(null);
         Printer p= new Printer(null);
         Suma s=new Suma(4,2);
+
+        Operaciones r = new Operaciones(25,32);
     //dos metodos
+    r.sumar();
    s.sumar(); 
    Scanner teclado = new Scanner(System.in);
         System.out.println("digite su nombre :");

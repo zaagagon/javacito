680374ca7a36fe5e4983f9aa218bd68070b09def
System Administrator <zaagagon@gmail.com>
2022-04-10 00:04:40 -0500

    objeto printer

--- Operaciones.java
+++ Operaciones.java
@@ -1,5 +1,22 @@
 public class Operaciones {
 
     //atributos de la clase
+
+    int n1,n2,result;
+
+    //constructor clase operaciones
+
+    Operaciones(int x,int y) {
+
+        n1=x;
+        n2=y;
+    }
     
+   // metodo de la clase 
+   public void sumar(){
+       result=n1+n2;
+       //creamos objeto de la clase Printer
+       Printer mostrar = new Printer(null);
+       mostrar.printer("Resultado operación : ", result);
+   }
 }

63e85cc442e2f1525eb4d6619c68cca67d8e0019
System Administrator <zaagagon@gmail.com>
2022-04-10 23:43:48 -0500

    metodo sobrecargado solucion

--- PruebaOperaciones.java
+++ PruebaOperaciones.java
@@ -6,7 +6,11 @@ public static void main(String a[] ) {
     Printer k = new Printer(null);
     String name ="yolis";
     //invocamos metodo printer
-k.printer("Probando operaciones con metodo de la clase Printer");
+    //problema no recibiendo dos parametros
+    //creamos otro metodo para que reciba un solo parametro
+    //sobrecarga de parametros
+    //revisar ...
+k.printer("Probando operaciones con metodo de la clase Printer un parametro");
     System.out.println("Hola zag");
     
     

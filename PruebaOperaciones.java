e5bed8331aa75b3f482c8283dfd766a40dce4c93
System Administrator <zaagagon@gmail.com>
2022-04-11 23:19:14 -0500

    menu java

--- PruebaOperaciones.java
+++ PruebaOperaciones.java
@@ -29,9 +29,43 @@ public static void main(String a[] ) {
     k.printer("Bienvenido : ", nombre);
     k.printer("Probando operaciones con metodo de la clase Printer un parametro");
     System.out.println("Aprendiendo clases invocacion de metodos");
-    
+
+    Scanner sn = new Scanner(System.in);
+       boolean salir = false;
+       int opcion; //Guardaremos la opcion del usuario
+       
+       while(!salir){
+           
+           System.out.println("1. Opcion 1");
+           System.out.println("2. Opcion 2");
+           System.out.println("3. Opcion 3");
+           System.out.println("4. Salir");
+           
+           System.out.println("Escribe una de las opciones");
+           opcion = sn.nextInt();
+           
+           switch(opcion){
+               case 1:
+                   System.out.println("Has seleccionado la opcion 1");
+                   break;
+               case 2:
+                   System.out.println("Has seleccionado la opcion 2");
+                   break;
+                case 3:
+                   System.out.println("Has seleccionado la opcion 3");
+                   break;
+                case 4:
+                   salir=true;
+                   break;
+                default:
+                   System.out.println("Solo números entre 1 y 4");
+           }
+           
+       }
+       
+    }
     
 }
 
     
-}
+

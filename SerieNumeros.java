e4a2d4310ccf1d156c46021b197fe5b1856d0934
zaagagon <zaagagon@gmail.com>
2023-10-26 23:55:43 -0500

    serie numeros

--- SerieNumeros.java
+++ SerieNumeros.java
@@ -1,10 +1,12 @@
  
     
     public class SerieNumeros {
+
+        //metodo principal
         public static void main(String[] args){
             //bucle para imprimir numeros
             // programa creado con github noviembre 2021 -- hulk cap cambios en gitconfig--
-            System.out.println("Numeros del 1 al 10");
+            System.out.println(" Programa Numeros del 1 al 10");
             for(int i=1;i<=10;i++){
                 System.out.println(i);
             }

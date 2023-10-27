121c6703c4795640ed6a51b720108f782737395a
zaagagon <zaagagon@gmail.com>
2023-10-26 23:49:28 -0500

    retomando java 2023

--- saludo.java
+++ saludo.java
@@ -5,7 +5,7 @@ public class saludo
     //metodo principal
         public static void main(String args[])
     {
-        System.out.println("Programando en java con git");
+        System.out.println(" ** Programando en java con git 2023 ** ");
         for(int i = 0; i <= 12; i++)
         //tabla del 12?
         {

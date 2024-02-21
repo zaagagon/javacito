eee273e86a82ba9cfd5e1bbeb88e262953f9fd76
zaagagon <zaagagon@gmail.com>
2024-02-21 00:47:49 -0500

    prueba funcion principal

--- holita2.java
+++ holita2.java
@@ -3,7 +3,7 @@ public class holita2 {
 public static void main(String[] args) {
     int num1  = 4;
     int num2  = 5;
-    System.out.println("resultado operacion: "+num1 + num2);
+    System.out.println("resultado operacion: "+(num1 + num2));
 }
     
 }

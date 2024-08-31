d1be2ab14b208d323b4290d59c34de476a560db9
zaagagon <zaagagon@gmail.com>
2024-08-30 19:47:20 -0500

    clases mejoradas

--- programita/Main.java
+++ programita/Main.java
@@ -6,11 +6,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Profesor profesor1= new Profesor("Peter", "P001");
+        Profesor profesor1= new Profesor("Peter", "P001","poo","profesor1");
+        Profesor profesor2= new Profesor("Banner", "P002","programacion IV","xx");
 
        
 
-        profesor1.mostrarInfo();  // Llama al método para mostrar la información
+       // profesor1.mostrarInfo();  // Llama al método para mostrar la información
+     //   profesor2.mostrarInfo();  // Llama al método para mostrar la información
     
     }
 }
--- programita/Profesor.java
+++ programita/Profesor.java
@@ -7,6 +7,7 @@ public class Profesor {
     // Atributos privados de la clase Profesor
     private String nombre;      // Nombre del profesor
     private String idProfesor;  // Identificador único del profesor
+    private String materia;  // Identificador único del profesor
 
     /**
      * Constructor de la clase Profesor.
@@ -14,16 +15,25 @@ public class Profesor {
      * @param nombre     Nombre del profesor.
      * @param idProfesor Identificador único del profesor.
      */
-    public Profesor(String nombre, String idProfesor) {
+    public Profesor(String nombre, String idProfesor,String materia, String referencia) {
         this.nombre = nombre;
         this.idProfesor = idProfesor;
+        this.materia=materia;
+        System.out.println("objeto creado : " + referencia+ this.nombre);
+        //Podemos llamar el metodo desde el constructor especial
+        mostrarInfo();
+        mostrar();
     }
     
     // Aquí podría agregar más métodos personalizados, si lo desea.
 
     // Método para mostrar la información del profesor
     public void mostrarInfo() {
-        System.out.println("Profesor: " + nombre + " (ID: " + idProfesor + ")");
+        System.out.println("Profesor: " + nombre + " (ID: " + idProfesor + ") " + materia);
+    }
+
+    public void mostrar() {
+        System.out.println(String.format("Profesor: %s\nID: %s\nmateria: %s", nombre, idProfesor, materia));
     }
 }
 

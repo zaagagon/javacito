b891f08100c4aee0bf54772a1ca1bd2d2d5cf788
zaagagon <zaagagon@gmail.com>
2024-09-23 19:18:04 -0500

    preparando clase principal CalculadoraApp

---
+++ Calculadora/CalculadoraApp.java
@@ -0,0 +1,13 @@
+package Calculadora;
+import Calculadora.Calculadora;
+import Suma;
+public class CalculadoraApp {
+    
+    public static void main(String[] args) {
+        //creamos un objeto de la clase suma
+        Calculadora calc=new Suma();
+        double resultado=calc.operar(4, 5);
+
+        System.out.println(resultado);
+    }
+}
